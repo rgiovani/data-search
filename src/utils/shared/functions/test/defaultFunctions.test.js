b5906ac1bdb6007299cee5f501a9516ecbf224dd
rgiovani <ronaldo-giovani07@hotmail.com>
2020-10-17 19:02:37 -0300

    1.1.4 - updating defaultFunctions.test

--- src/utils/shared/functions/test/defaultFunctions.test.js
+++ src/utils/shared/functions/test/defaultFunctions.test.js
@@ -1,16 +1,6 @@
 import { getCurrentTimeString, treatString, verifyParam } from '../defaultFunctions';
 import { isArray, isString } from '../validateTypes';
 
-const main = {
-    array: [{
-        id: 1,
-        make: {
-            age: ['2018', '2019', [{ a: 'isso' }]],
-
-        }
-    }],
-}
-
 describe('Validating \'initializeVariables\' function', () => {
     test('it should return \'false\' in \'verifyParam\' function', () => {
         expect(verifyParam('age', 'name', 'age')).toBe(false);
